3bee24655f45c20f870a8ab91222b71bcd097144
Devin Veasna <veasnadt@vcu.edu>
2024-12-05 19:59:08 -0500

    Designed UI for rock paper scissors and converted console outputs into DOM methods.

--- script.js
+++ script.js
@@ -1,74 +1,91 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+  getHumanChoice();
+});
+
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3);
-    switch (randomNumber) {
-      case 0:
-        return 'rock';
-      case 1:
-        return 'paper';
-      case 2:
-        return 'scissors';
-    }
+  let randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
   }
+}
   
-  function getHumanChoice() {
-    let humanChoice = prompt('Please choose rock, paper, or scissors.');
-    return humanChoice.toLowerCase();
-  }
-  
-  humanScore = 0;
-  computerScore = 0;
-  
-  function playRound(humanSelection, computerSelection) {
-    if (humanSelection === computerSelection) {
-      console.log('It\'s a tie! Score: ' + computerScore + ' - ' + humanScore);
-    } 
-    else if (humanSelection === 'rock') {
-      if (computerSelection === 'paper') {
-        computerScore++;
-        console.log('Computer wins! Score: ' + computerScore + ' - ' + humanScore);
-      } 
-      else {
-        humanScore++;
-        console.log('Human wins! Score: ' + computerScore + ' - ' + humanScore);
-      }
-    }
-    else if (humanSelection === 'paper') {
-      if (computerSelection === 'scissors') {
-        computerScore++;
-        console.log('Computer wins! Score: ' + computerScore + ' - ' + humanScore);
-      } 
-      else {
-        humanScore++;
-        console.log('Human wins! Score: ' + computerScore + ' - ' + humanScore);
-      }
-    }
-    else if (humanSelection === 'scissors') {
-      if (computerSelection === 'rock') {
-        computerScore++;
-        console.log('Computer wins! Score: ' + computerScore + ' - ' + humanScore);
-      } 
-      else {
-        humanScore++;
-        console.log('Human wins! Score: ' + computerScore + ' - ' + humanScore);
-      }
-    }
-  }
-  
-  function playGame() {
-    for (let i = 0; i < 5; i++) {
-      let humanSelection = getHumanChoice();
-      let computerSelection = getComputerChoice();
-      playRound(humanSelection, computerSelection);
-    }
-    if (computerScore > humanScore) {
-      console.log('Computer wins the game!');
-    } 
-    else if (computerScore < humanScore) {
-      console.log('Human wins the game!');
+function getHumanChoice() {
+  const rockBtn = document.getElementById('rock-btn');
+  const paperBtn = document.getElementById('paper-btn');
+  const scissorsBtn = document.getElementById('scissors-btn');
+  rockBtn.addEventListener('click', () => {
+    playRound('rock', getComputerChoice());
+  });
+  paperBtn.addEventListener('click', () => {
+    playRound('paper', getComputerChoice());
+  });
+  scissorsBtn.addEventListener('click', () => {
+    playRound('scissors', getComputerChoice());
+  });
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanSelection, computerSelection) {
+  if (humanSelection === computerSelection) {
+    displayResult('It\'s a tie! Score: ' + computerScore + ' - ' + humanScore);
+  } 
+  else if (humanSelection === 'rock') {
+    if (computerSelection === 'paper') {
+      computerScore++;
+      displayResult('Computer wins! Score: ' + computerScore + ' - ' + humanScore);
     } 
     else {
-      console.log('It\'s a tie game!');
+      humanScore++;
+      displayResult('Human wins! Score: ' + computerScore + ' - ' + humanScore);
     }
   }
-  
-  playGame();
+  else if (humanSelection === 'paper') {
+    if (computerSelection === 'scissors') {
+      computerScore++;
+      displayResult('Computer wins! Score: ' + computerScore + ' - ' + humanScore);
+    } 
+    else {
+      humanScore++;
+      displayResult('Human wins! Score: ' + computerScore + ' - ' + humanScore);
+    }
+  }
+  else if (humanSelection === 'scissors') {
+    if (computerSelection === 'rock') {
+      computerScore++;
+      displayResult('Computer wins! Score: ' + computerScore + ' - ' + humanScore);
+    } 
+    else {
+      humanScore++;
+      displayResult('Human wins! Score: ' + computerScore + ' - ' + humanScore);
+    }
+  }
+}
+
+function displayResult(result) {
+  const resultDiv = document.getElementById('result');
+  const resultP = document.createElement('p');
+  resultP.textContent = result;
+  resultDiv.appendChild(resultP);
+
+  if (computerScore === 5) {
+    const resultP = document.createElement('p');
+    resultP.textContent = 'Computer wins the game!';
+    resultDiv.appendChild(resultP);
+    computerScore = 0;
+    humanScore = 0;
+  }
+  else if (humanScore === 5) {
+    const resultP = document.createElement('p');
+    resultP.textContent = 'Human wins the game!';
+    resultDiv.appendChild(resultP);
+    computerScore = 0;
+    humanScore = 0;
+  }
+}
